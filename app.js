0d7a408787cf3541744944756353f0df14b966a0
Cryptogirlx <zsofia.tubel@yahoo.com>
2021-01-21 23:54:51 +0100

    stuff

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require("express");
-const createError = require('http-errors');
+const createError = require("http-errors");
 const errorHandlingMiddleware = require("./middleware/error-handling.middleware");
 const app = express();
-const PORT = process.env.PORT || 8000; 
-const mainRouter = require('./routes');
+const PORT = process.env.PORT || 8000;
+const mainRouter = require("./routes");
 
 app.use(express.json());
-app.use(mainRouter);
+app.use("/api", mainRouter);
 
-app.use('*', (req, res, next) =>{
-    
-    next(createError(404, "This is not found"));
-})
+app.use("*", (req, res, next) => {
+  next(createError(404, "This is not found"));
+});
 
 app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
-    console.log(`app running on port ${PORT} `);
+  console.log(`app running on port ${PORT} `);
 });
